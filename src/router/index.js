682042dc9f0ccfc845944b7996a3128b19bebd3d
Akrem Jomaa <jomaakrem@gmail.com>
2023-02-08 15:50:34 +0100

    add tokenStore

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import HomeView from '../views/HomeView.vue';
 import AdminDomainListView from '../views/AdminDomainListView.vue';
 import oneDomainAdminView from '../views/OneAdminDomainView.vue';
 import DisplayDeviceView from '../views/DisplayDeviceView.vue';
+// import UpdateDeviceView from '../views/UpdateDeviceView.vue';
+import SettingsView from '../views/SettingsView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,8 +29,16 @@ const router = createRouter({
       name: 'DisplayDevice',
       component: DisplayDeviceView,
     },
-
-    DisplayDeviceView,
+    // {
+    //   path: '/UpdateDevice',
+    //   name: 'UpdateDevice',
+    //   component: UpdateDeviceView,
+    // },
+    {
+      path: '/Settings',
+      name: 'Settings',
+      component: SettingsView,
+    },
   ],
 });
 
