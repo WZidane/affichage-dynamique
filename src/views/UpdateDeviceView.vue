3717f7f9ab0e4d4b9dedfbc576e4cae5aa854c2d
StarCobra <damienpoirot@yahoo.com>
2023-02-09 14:19:29 +0100

    Amélioration du formulaire pour choisir son TOKEN

--- src/views/UpdateDeviceView.vue
+++ src/views/UpdateDeviceView.vue
@@ -1,4 +1,3 @@
-
 <script setup>
 import { onMounted, reactive } from "@vue/runtime-core";
 import { inject } from "@vue/runtime-core";
@@ -14,52 +13,52 @@ let state = reactive({
     ecrans: [],
     visible: false,
     NameDevice: "",
-    Devices: []
+    Devices: [],
+    selectedTokenId: "",
+    otherDevices:[]
 });
 
 
 onMounted(() => {
     getDeviceInformation();
+    getDevices();
 })
 
 function getDeviceInformation() {
     axios.get(`${BASE}${OBJ}${TOKEN}?fields=Nom_Dispositif,Sequences.Ordre_Sequence,Sequences.Sequence_id.Ecrans.Ecran_id.Donnees,Sequences.Sequence_id.Ecrans.Ordre_Ecran,Sequences.Sequence_id.Ecrans.Ecran_id.Duree,Domaine.Nom_Domaine,Ecrans.Ecran_id.Donnees,Ecrans.Ecran_id.Duree`).then(response => {
-        state.Device = response.data
-        state.NameDevice = state.Device.data.Nom_Dispositif
+        state.Device = response.data;
+        state.NameDevice = state.Device.data.Nom_Dispositif;
         state.ecrans = state.Device.data.Ecrans;
         state.sequences = state.Device.data.Sequences;
-        state.Domain = state.Device.data.Domaine
-        console.log('Ligne 1 : ')
-        console.log(state.NameDevice)
-        console.log('Ligne 2 : ')
-        console.log(state.ecrans)
-        console.log('Ligne 3 : ')
-        console.log(state.sequences)
-        console.log('Ligne 4 : ')
-        console.log(state.Domain.Nom_Domaine)
-    })
+        state.Domain = state.Device.data.Domaine;
+        state.selectedTokenId = TOKEN;
+    });
 }
 function getDevices() {
-    axios.get(`${BASE}${OBJ}${TOKEN}??fields=id`).then(response => {
-        state.Devices = response.data
-    })
+    axios.get(`${BASE}${OBJ}??fields=id`).then(response => {
+        state.Devices = response.data;
+        state.otherDevices = state.Devices.data
+    });
 }
-
+function update(valeur) {
+    TOKEN = valeur;
+}
 
 </script>
 <template>
     <h1>Page UpdateDevice</h1>
     <h2>Vous êtes actuellement sur le domaine: {{ state.Domain.Nom_Domaine }}</h2>
     <h3>L'url : {{ BASE }}{{ OBJ }}</h3>
-    <h3>TOKEN du dispositif d'affichage : 
-        <select name="Token">
-            <option value="{{TOKEN}}">{{TOKEN}}</option>
-        </select>
+    <h3>TOKEN du dispositif d'affichage :
+        <form id="formulaireNewToken" @submit.prevent="update">
+            <select id="NewTOKEN" :v-model="TOKEN" name="NewTOKEN">
+                <option v-for="token in state.otherDevices" :key="token.id" :value="token.id">{{ token.id }}</option>
+            </select>
+            <button>Valider changement</button>
+            <!-- <input type="submit" value="Valider changement"> -->
+        </form>
     </h3>
-
 </template>
 <style scoped>
 
 </style>
-
-
