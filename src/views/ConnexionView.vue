529b4b8acbcc0f2b9f455bc5dadfff3cc7de62cd
leosix10 <leo.six1@etu.univ-lorraine.fr>
2023-02-09 17:49:17 +0100

    init CSS

--- src/views/ConnexionView.vue
+++ src/views/ConnexionView.vue
@@ -31,34 +31,35 @@ function validationFormulaire() {
 </script>
 
 <template>
-  <div class="columns">
-    <div class="column is-4 is-offset-4">
 
-      <form class="box" @submit.prevent="validationFormulaire">
+  <div class="connexionView">
 
-        <h1 class="title has-text-centered">SE CONNECTER</h1>
+    <h1>SE CONNECTER</h1>
 
-        <div class="field">
-          <label class="label">E-Mail</label>
-          <input class="input" v-model="user.email" type="email" placeholder="email@domaine.com">
+    <form @submit.prevent="validationFormulaire">
+
+      <div class="field">
+        <label class="label">E-Mail</label>
+        <input class="input" v-model="user.email" type="email" placeholder="email@domaine.com">
+      </div>
+
+      <div class="field">
+        <label class="label">Mot de passe</label>
+        <input class="input" v-model="user.password" type="password" placeholder="Your password">
+      </div>
+
+      <div class="field is-grouped">
+        <div class="control">
+          <button>
+            <router-link to="/" >Annuler</router-link>
+          </button>
         </div>
-
-        <div class="field">
-          <label class="label">Mot de passe</label>
-          <input class="input" v-model="user.password" type="password" placeholder="Your password">
+        <div class="control">
+          <button class="is-primary">Se connecter</button>
         </div>
+      </div>
+    </form>
 
-        <div class="field is-grouped">
-          <div class="control">
-            <button class="button is-primary">Se connecter</button>
-          </div>
-          <div class="control">
-            <router-link to="/" class="button">Annuler</router-link>
-          </div>
-        </div>
-      </form>
-
-    </div>
   </div>
 
 </template>
