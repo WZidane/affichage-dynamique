0fd96a2244d515b331d9373e86f10467c3e83723
WZidane <124033462+WZidane@users.noreply.github.com>
2023-02-10 16:13:02 +0100

    Revert "Develop sessions"

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import HomeView from '../views/HomeView.vue';
 import DisplayDeviceView from '../views/DisplayDeviceView.vue';
 import UpdateDeviceView from '../views/UpdateDeviceView.vue';
 import SettingsView from '../views/SettingsView.vue';
-import ConnexionView from "@/views/ConnexionView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,16 +27,6 @@ const router = createRouter({
       path: '/Settings',
       name: 'Settings',
       component: SettingsView,
-    },
-    {
-      path: '/se-connecter',
-      name: 'se-connecter',
-      component: ConnexionView,
-    },
-    {
-      path: '/DisplayDevice',
-      name: 'DisplayDevice',
-      component: DisplayDeviceView,
     },
   ],
 });
--- src/stores/token.js
+++ src/stores/token.js
@@ -5,12 +5,10 @@ export const useTokenStore = defineStore(
   'token',
   () => {
     const state = reactive({
-      BASE: 'https://74b3jzk3.directus.app/items/',
-      TOKEN: '2f998884-e4c3-4cb3-9ae5-6bb2a4f43376',
-      OBJ: 'Dispositif_Affichage/',
-      DOMAIN: null
+      BASE: '',
+      TOKEN: '',
+      OBJ: '',
     });
-    /*
     function setDefaultBasicUrl() {
       state.BASE = 'https://74b3jzk3.directus.app/items/';
     }
@@ -20,9 +18,7 @@ export const useTokenStore = defineStore(
     function setDefaultToken() {
       state.TOKEN = 'a5e3c94f-c480-414b-b092-e6ce0f55c3ca';
     }
-     */
-
-    return { state /*,setDefaultBasicUrl, setDeviceObj, setDefaultToken*/ };
+    return { state, setDefaultBasicUrl, setDeviceObj, setDefaultToken };
   },
   {
     persist: true,
