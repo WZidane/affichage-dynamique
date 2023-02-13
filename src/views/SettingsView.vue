466630dff9b3f541affa77d145520daddcc22636
leosix10 <leo.six1@etu.univ-lorraine.fr>
2023-02-13 17:27:31 +0100

    button CSS fixed

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -75,8 +75,8 @@ async function getDeviceInformation() {
     </div>
     <!-- <h3>L'url : {{ token.state.BASE }}{{ token.state.OBJ }}</h3> -->
 
-    <button class="is-primary">
-        <RouterLink to="UpdateDevice">Mettre à jour</RouterLink>
+    <button class="is-primary" @click="$router.push('UpdateDevice')">
+        Mettre à jour
     </button>
 </template>
 <style scoped>
